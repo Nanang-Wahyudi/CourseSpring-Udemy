592902be34ed284f2b4a5722d516865923a113df
Diki Center <dikimistak@gmail.com>
2023-03-01 15:36:02 +0700

    First Commit Code

---
+++ BAG 5a - Dependency Injection & Inversion Of Control/src/main/java/com/BookCatalog/App.java
@@ -0,0 +1,13 @@
+package com.BookCatalog;
+
+import com.BookCatalog.Domain.Author;
+
+public class App {
+	public static void main(String[] args) {
+		
+	//	Pembuatan Object Dengan Prinsip Dependency Hardcode
+		Author author = new Author(); // Pembuatan Object Yang Kurang Tepat
+		
+	
+	}
+}
